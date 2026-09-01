10d19a0e074f28a55451396d429247c6398c3545
sozascript <tesakungg23@gmail.com>
2026-09-01 09:47:15 +0700

    Configure Railway deployment and fix Discord command registration

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
     ],
     songStatus: true
   },
-  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
-  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID || "",
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
 }
