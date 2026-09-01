5310a77fb35b972e0ddea79d8d1e7f1d3ef4e3a9
sozascript <tesakungg23@gmail.com>
2026-09-01 14:09:32 +0700

    Update activity status in config.js

--- config.js
+++ config.js
@@ -4,10 +4,7 @@ module.exports = {
   ownerId: '1004206704994566164',
   status: {
     rotateDefault: [
-      { name: 'Netflix', type: ActivityType.Watching },
-      { name: 'GTA VI', type: ActivityType.Playing },
-      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
-      { name: 'Spotify', type: ActivityType.Custom },
+      { name: 'soza', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
     ],
     songStatus: true
   },
